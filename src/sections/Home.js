e02cdd5fe9b2d3223c2e2b99d4360eef24cae6f2
Talha <talha.shshid@toolshedservices.com>
2024-07-30 22:28:28 -0700

    enablimg-mails-commit

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -48,27 +48,28 @@ const HomePage = () => {
     // onClose();
 
     // }
-    setTimeout(() => {
-      setLoading(false);
-      setShowSuccessModal(true);
-      
-    }, 2000);
-    // Send the email using EmailJS
-    // emailjs.send(
-    //   process.env.REACT_APP_EMAILJS_SERVICE_ID, // Service ID from your EmailJS account
-    //   process.env.REACT_APP_EMAILJS_TEMPLATE_ID, // Template ID from your EmailJS account
-    //   { ...emailParams },
-    //   process.env.REACT_APP_EMAILJS_USER_ID // User ID from your EmailJS account
-    // )
-    // .then((response) => {
-    //   console.log('Email sent successfully:', response.status, response.text);
+    // setTimeout(() => {
     //   setLoading(false);
     //   setShowSuccessModal(true);
       
-    // })
-    // .catch((err) => {
-    //   console.error('Failed to send email:', err);
-    // });
+    // }, 2000);
+    
+    // Send the email using EmailJS
+    emailjs.send(
+      process.env.REACT_APP_EMAILJS_SERVICE_ID, // Service ID from your EmailJS account
+      process.env.REACT_APP_EMAILJS_TEMPLATE_ID, // Template ID from your EmailJS account
+      { ...emailParams },
+      process.env.REACT_APP_EMAILJS_USER_ID // User ID from your EmailJS account
+    )
+    .then((response) => {
+      console.log('Email sent successfully:', response.status, response.text);
+      setLoading(false);
+      setShowSuccessModal(true);
+      
+    })
+    .catch((err) => {
+      console.error('Failed to send email:', err);
+    });
   };
 
 
